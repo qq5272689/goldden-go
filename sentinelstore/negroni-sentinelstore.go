9c7a242eedd3ef4200a933790674f9f396e65032
hjd <hjd_2008@foxmail.com>
2019-01-16 17:16:57 +0800

    add sentinelstore

---
+++ sentinelstore/negroni-sentinelstore.go
@@ -0,0 +1,28 @@
+package sentinelstore
+
+import (
+	nSessions "github.com/goincremental/negroni-sessions"
+	gSessions "github.com/gorilla/sessions"
+	"github.com/mediocregopher/radix"
+)
+
+//New returns a new Sentinel store
+func NewNegroniSentinelStore(Sentinel *radix.Sentinel,sessionExpire int, keyPairs ...[]byte) (nSessions.Store) {
+	store := NewSentinelStore(Sentinel,sessionExpire, keyPairs...)
+	return &NegroniSentinleStore{store}
+}
+
+type NegroniSentinleStore struct {
+	*SentinleStore
+}
+
+func (c *NegroniSentinleStore) Options(options nSessions.Options) {
+	c.SentinleStore.Options = &gSessions.Options{
+		Path:     options.Path,
+		Domain:   options.Domain,
+		MaxAge:   options.MaxAge,
+		Secure:   options.Secure,
+		HttpOnly: options.HTTPOnly,
+	}
+}
+
