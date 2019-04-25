0b33ee7fc237da30918cf056a1a3119cca98aafd
hjd <hjd_2008@foxmail.com>
2019-04-25 11:08:45 +0800

    add idle timeout

--- goredisSentinelstore/sentinel.go
+++ goredisSentinelstore/sentinel.go
@@ -13,7 +13,7 @@ func RedisInit(rc *RedisConf) (*redis.Client, error) {
 	timeout := time.Millisecond * time.Duration(rc.TimeOut)
 	fo := &redis.FailoverOptions{MasterName: rc.MasterName, SentinelAddrs: rc.Sentinels,
 		Password: rc.Password, DB: rc.DB, DialTimeout: timeout, ReadTimeout: timeout,
-		WriteTimeout: timeout, PoolTimeout: timeout, IdleTimeout: timeout, PoolSize: rc.Pool}
+		WriteTimeout: timeout, PoolTimeout: timeout, IdleTimeout: timeout * 1000, PoolSize: rc.Pool}
 	c := redis.NewFailoverClient(fo)
 	r, err := c.Ping().Result()
 	if r != "PONG" || err != nil {
