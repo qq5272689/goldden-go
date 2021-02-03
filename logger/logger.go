b4fceba5ddb0e7f4741f5bcebb7466da17dc28fc
haungjindie <hjd_2008@foxmail.com>
2021-02-03 11:56:30 +0800

    add logger lock

--- logger/logger.go
+++ logger/logger.go
@@ -4,12 +4,16 @@ import (
 	"github.com/qq5272689/goutils/zap_logger"
 	"go.uber.org/zap"
 	"runtime/debug"
+	"sync"
 )
 
 var logger *zap.Logger
 var Closer zap_logger.Closer
+var mu = new(sync.Mutex)
 
 func init() {
+	mu.Lock()
+	defer mu.Unlock()
 	logger, _ = zap.NewDevelopment()
 	Closer = func() error {
 		return logger.Sync()
@@ -17,6 +21,8 @@ func init() {
 }
 
 func LoggerInit(env, dir, service, when string) {
+	mu.Lock()
+	defer mu.Unlock()
 	logger.Sync()
 	var err error
 	if env == "dev" || env == "local" {
@@ -33,6 +39,16 @@ func LoggerInit(env, dir, service, when string) {
 
 func GetLogger() *zap.Logger {
 	return logger
+}
+
+func SetLogger(logger2 *zap.Logger) {
+	mu.Lock()
+	defer mu.Unlock()
+	logger.Sync()
+	logger = logger2
+	Closer = func() error {
+		return logger.Sync()
+	}
 }
 
 func Debug(msg string, fields ...zap.Field) {
